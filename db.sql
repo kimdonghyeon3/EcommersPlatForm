b41ad29ec292004471bae0e093cb45939aa344c8
kimdonghyeon3 <sh2115521@naver.com>
2022-10-11 13:41:01 +0900

    feat : 개발환경 조성 test, dev, prod 나누기

---
+++ db.sql
@@ -0,0 +1,9 @@
+# 테스트 DB 생성
+DROP DATABASE IF EXISTS ecommercePlatForm_test;
+CREATE DATABASE ecommercePlatForm_test;
+USE ecommercePlatForm_test;
+
+# 개발 DB 생성
+DROP DATABASE IF EXISTS ecommercePlatForm_dev;
+CREATE DATABASE ecommercePlatForm_dev;
+USE ecommercePlatForm_dev;
---
+++ src/main/java/com/example/ecommerceplatform/app/base/initData/InitDataBefore.java
@@ -0,0 +1,7 @@
+package com.example.ecommerceplatform.app.base.initData;
+
+public interface InitDataBefore {
+    default void before() {
+
+    }
+}
