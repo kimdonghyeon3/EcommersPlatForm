cc4e3856722b09de25f0a925acb486e32fa99026
kimdonghyeon3 <sh2115521@naver.com>
2022-10-12 13:51:31 +0900

    feat : 결제성공시 결제 완료 처리 리다이렉트

--- src/main/java/com/example/ecommerceplatform/app/order/controller/OrderController.java
+++ src/main/java/com/example/ecommerceplatform/app/order/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.example.ecommerceplatform.app.order.entity.Order;
 import com.example.ecommerceplatform.app.order.exception.OrderIdNotMatchedException;
 import com.example.ecommerceplatform.app.order.service.OrderService;
 import com.example.ecommerceplatform.app.security.dto.MemberContext;
+import com.example.ecommerceplatform.util.Ut;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -99,10 +100,9 @@ public class OrderController {
                 "https://api.tosspayments.com/v1/payments/" + paymentKey, request, JsonNode.class);
 
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
-            JsonNode successNode = responseEntity.getBody();
-            model.addAttribute("orderId", successNode.get("orderId").asText());
-            String secret = successNode.get("secret").asText(); // 가상계좌의 경우 입금 callback 검증을 위해서 secret을 저장하기를 권장함
-            return "order/success";
+            orderService.payByTossPayments(order);
+
+            return "redirect:/order/%d?msg=%s".formatted(order.getId(), Ut.url.encode("결제가 완료되었습니다."));
         } else {
             JsonNode failNode = responseEntity.getBody();
             model.addAttribute("message", failNode.get("message").asText());
